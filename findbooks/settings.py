f40e45e1c754625a5b8b365cec836d5c72e6d7ea
Thupden Bhutia <47317869+thupden@users.noreply.github.com>
2020-08-02 14:40:41 +0530

    Update settings.py

--- findbooks/settings.py
+++ findbooks/settings.py
@@ -54,8 +54,8 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thupdenb281199@gmail.com'
-EMAIL_HOST_PASSWORD = 'Kalimpong'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
 
 
 
